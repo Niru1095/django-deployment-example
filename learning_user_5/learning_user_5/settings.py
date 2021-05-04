17407d8d4ba6393e853be8b077701fa635af4592
Niru1095 <44478409+Niru1095@users.noreply.github.com>
2021-05-04 17:10:22 +0530

    ALLOWED_HOSTS = ['*']

--- learning_user_5/learning_user_5/settings.py
+++ learning_user_5/learning_user_5/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-6jnksa+q4!7tj@o6cu00v*5*2mwz(7t#i1+vti7@54ko@j7hv!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
